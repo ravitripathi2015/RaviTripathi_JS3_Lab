e7df4c9848aee1f5647a76cefb79a75fe22991f4
ravitripathi2015 <30433413+ravitripathi2015@users.noreply.github.com>
2023-07-23 18:55:19 +0530

    Add files via upload

---
+++ forecast.js
@@ -0,0 +1,47 @@
+//get reference of input box search
+const searchbox=document.querySelector('.search-box');
+searchbox.addEventListener('keypress',setQuery);
+function setQuery(evt){
+  if(evt.keyCode==13){
+    getResults(searchbox.value);
+  }
+}
+const apikey="52d06dd23aa758a165fc1b106a2dcdc3";
+function getResults(query){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apikey}&units=metric`)
+  .then(weather=>{return weather.json();})
+  .then(response=>{console.log(response);
+    displayResults(response);
+  });
+}
+
+function displayResults (weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  console.log(now);
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
